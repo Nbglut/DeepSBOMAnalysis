7daab92d538ee21498f90329bc1241375960d25d
NBGlut <nbglut@wm.edu>
2025-02-18 08:46:20 -0500

    Added code to get SBOM based on repo's link and store the json

---
+++ SBOM.py
@@ -0,0 +1,60 @@
+"""
+SBOM.py
+
+This class simply uses the GitHub API to get the SBOM of a given project and stores the json
+
+
+@Author Nicolette Glut
+File Created On 2-18-2025
+"""
+
+import os
+import requests
+import json
+import sys
+
+class SBOM:
+    """
+    Class to add a media-related bug into a mutated repository.
+
+    """
+    def __init__(self, repo):
+        self.repo=repo
+        self.SBOMjson={}
+
+
+
+    def findJson(self):
+        """
+        Retrieves the media used for the mutation
+
+        Returns:
+            self.media: string name of the file
+        """
+        items= self.repo.split("/")
+        api_request="https://api.github.com/repos/" + items[3] +"/" + items[4]+ "/dependency-graph/sbom"
+        print(api_request)
+        response = requests.get(api_request)
+        print(response.status_code) # Print the status code
+        self.SBOMjson=response.json() 
+
+
+
+    def getJson(self):
+        """
+           Returns the SBOM json
+        """
+        return self.SBOMjson
+
+
+      
+
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+         print("No repo given")
+    test_SBOM = SBOM(sys.argv[1])
+    test_SBOM .findJson()
+
+    print(test_SBOM.getJson())
+
