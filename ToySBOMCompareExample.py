2623995b2f0497070aa1cf1e2af5413aab3234b5
NBGlut <nbglut@wm.edu>
2025-02-18 23:44:29 -0500

    Basic Comparision Implementation

--- ToySBOMCompareExample.py
+++ ToySBOMCompareExample.py
@@ -13,6 +13,9 @@ import requests
 import json
 import sys
 from SBOM import SBOM
+from deepdiff import DeepDiff
+import copy
+
 
 
 class ToySBOMCompareExample:
@@ -37,9 +40,20 @@ class ToySBOMCompareExample:
         """
         self.ToySBOM.findJson()
         self.SBOMjsonTruth= self.ToySBOM.getJson()
+        self.SBOMjsonNonTruth= copy.deepcopy(self.SBOMjsonTruth)
+#placeholder
+       #Load SBOMjsonTruth into a map form
+        #data = json.loads(self.SBOMjsonNonTruth)
+	        #remove some stuff from SBOMjsonTruth here to get "ToySBOM"
+      
+        packagename="transformers"   #placeholder 
 
-		#remove some stuff from SBOMjsonTruth here to get "ToySBOM"
-
+	#delete package with "name"
+        self.SBOMjsonNonTruth["sbom"]["packages"] = [pkg for pkg in self.SBOMjsonNonTruth["sbom"]["packages"] if pkg["name"] != packagename]
+		#Chance to Remove a random package ([packages])
+		#chance to remove the license/change the license
+		#Chance to add a package(?)
+                
         
     def getTruthSBOM(self):
         """
@@ -52,10 +66,21 @@ class ToySBOMCompareExample:
 
 
 #COMPARE METHOD HERE
+#Maybe do Compare FIRST, then add more robust Toy example
 
+    def compareSBOMs(self):
+        output=""
+        difference = DeepDiff(self.SBOMjsonTruth,self.SBOMjsonNonTruth, ignore_order=True)    
+        if difference:
+           print("Differences found:")
+           print(difference)
+        else:
+           print("No differences found.")
 #COMPARE SBOMS, section by section
 #COMPARE Licenses (dataLicense)
-#Compare packages
+#Compare packages (missing packages, wrong info in packages, etc)
+#Add comparsions (like missing packages, wrong licenses, etc) to output
+
 
 
       
@@ -64,7 +89,10 @@ class ToySBOMCompareExample:
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
          print("No repo given")
+#https://github.com/microsoft/OmniParser
     test_SBOM = ToySBOMCompareExample(sys.argv[1])
     test_SBOM.findToySBOMs()
-    print(test_SBOM.getTruthSBOM())
+    test_SBOM.compareSBOMs()
+
+    #print(test_SBOM.getTruthSBOM())
 
